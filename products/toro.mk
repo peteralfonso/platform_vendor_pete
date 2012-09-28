1d9872969a6ab8901137ed7b0c60fd3359ec680a
Peter Alfonso <peteralfonsojr@gmail.com>
2012-09-27 22:47:35 -0400

    Update Toro build fingerprint to JRO03O

--- products/toro.mk
+++ products/toro.mk
@@ -11,7 +11,7 @@ PRODUCT_DEVICE := toro
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mysid BUILD_FINGERPRINT=google/mysid/toro:4.0.4/IMM76K/336647:user/release-keys PRIVATE_BUILD_DESC="mysid-user 4.0.4 IMM76K 336647 release-keys" BUILD_NUMBER=336647
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mysid BUILD_FINGERPRINT=google/mysid/toro:4.1.1/JRO03O/424425:user/release-keys PRIVATE_BUILD_DESC="mysid-user 4.1.1 JRO03O 424425 release-keys" BUILD_NUMBER=424425
 
 # Inherit common build.prop overrides
 -include vendor/pete/products/common_versions.mk
